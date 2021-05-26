34dcb03862737be1f14b8fcd197fab078ac62f22
Himujjal <himu@tuta.io>
2021-05-26 17:18:47 +0530

    updated

--- src/__tests__/sleep.js
+++
@@ -1,7 +0,0 @@
-export function wink() {
-  return new Promise(fn => process.nextTick(fn));
-}
-
-export function sleep(ms) {
-  return new Promise(fn => setTimeout(fn, ms));
-}
--- src/__tests__/token.js
+++
@@ -1,5 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-export function getToken(payload) {
-  return jwt.sign(payload, 'secret', {expiresIn: '10m'});
-}
