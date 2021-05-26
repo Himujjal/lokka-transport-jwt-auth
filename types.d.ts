dfae9b5697eec5f77419bb416f0294475773725f
Himujjal <himu@tuta.io>
2021-05-26 12:24:21 +0530

    updated

--- types.d.ts
+++ types.d.ts
@@ -1,14 +1,16 @@
-export interface IOption extends RequestInit {
+declare module "lokka-transport-jwt-auth" {
+  export interface IOption extends RequestInit {
     auth?: string;
     headers?: Record<string, string>;
     credentials?: RequestCredentials;
     handleErrors?: (errors: { message: string }[], data: any) => void;
-}
-export default class {
+  }
+  export default class {
     constructor(endpoint: string, refresh?: () => void);
     send<T = unknown>(
-        rawQuery: string,
-        variables: { [index: string]: any },
-        operationName: string
+      rawQuery: string,
+      variables: { [index: string]: any },
+      operationName: string
     ): Promise<T>;
+  }
 }
